a71b35085984441cbf3807ca6299a1c582cf4e76
Sarah Cnota <scnota@gmail.com>
2015-09-22 16:47:41 -0400

    add images, login form fields

--- login.js
+++ login.js
@@ -3,7 +3,10 @@
 var React = require('react-native');
 
 var {
+  Image,
   Text,
+  TextInput,
+  TouchableHighlight,
   StyleSheet,
   View
 } = React;
@@ -12,6 +15,22 @@ class Login extends React.Component{
   render() {
     return (
       <View style={styles.container}>
+        <Image style={styles.logo}
+          source={require('image!Octocat')} />
+        <Text style={styles.heading}>
+          Github Browser
+        </Text>
+        <TextInput style={styles.input}
+          placeholder="Github username" />
+        <TextInput style={styles.input}
+          placeholder="Github password"
+          secureTextEntry="true" />
+        <TouchableHighlight
+          style={styles.button}>
+          <Text style={styles.buttonText}>
+            Log In
+          </Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -20,7 +39,38 @@ class Login extends React.Component{
 var styles = StyleSheet.create({
   container: {
     backgroundColor: '#F5FCFF',
-    flex: 1
+    flex: 1,
+    paddingTop: 40,
+    alignItems: 'center',
+    padding: 10
+  },
+  logo: {
+    width: 66,
+    height: 55
+  },
+  heading: {
+    fontSize: 30,
+    marginTop: 10
+  },
+  input: {
+    height: 50,
+    marginTop: 10,
+    padding: 4,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48bbec'
+  },
+  button: {
+    height: 50,
+    backgroundColor: '#48BBEC',
+    alignSelf: 'stretch',
+    marginTop: 10,
+    justifyContent: 'center'
+  },
+  buttonText: {
+    fontSize: 22,
+    color: '#fff',
+    alignSelf: 'center',
   }
 });
 
